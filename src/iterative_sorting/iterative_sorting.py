c7a9e757f8c5f7138dd01380f253baa6da4013ae
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-07-16 13:18:54 -0500

    :art: Cleaned up range instances in for loops
    ranges beginning at 0 now no longer specify start of 0 (default)

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,7 +1,7 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(len(arr)):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
@@ -18,9 +18,8 @@ def selection_sort(arr):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-
-    for i in range(0, len(arr)):
-        for j in range(0, len(arr)-i-1):
+    for i in range(len(arr)):
+        for j in range(len(arr)-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
