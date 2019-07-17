253b3827545e14ea4964a953cf06524deeb65f07
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-07-16 19:21:28 -0500

    :sparkles: Count sort stretch complete and tests OK

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -27,6 +27,42 @@ def bubble_sort(arr):
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort(arr, maximum=-1):
+def count_sort(arr, maximum=200):
+    result = [0]*len(arr)
+    keys = [0]*maximum
 
-    return arr
+    for i in arr:
+        if i < 0:
+            return 'Error, negative numbers not allowed in Count Sort'
+        keys[i] += 1
+
+    # previous version, I tried to get too fancy by comparing the key with the next key
+    # for i, item in enumerate(keys):
+    #     if i != 0:
+    #         keys[i] += keys[i-1]
+    # keys = [0]+keys[:len(keys)-1]
+
+    x = 0
+    for i in range(len(keys)):
+        while keys[i] > 0:
+            result[x] = i
+            x += 1
+            keys[i] -= 1
+
+    return result
+
+
+arr1 = [0, 3, 8, 1, 2, 9, 6, 4, 5, 7]
+print(count_sort(arr1))
+
+# pseudocode
+# function countingSort(array, k) is
+#   count ← new array of k zeros
+#   for i = 1 to length(array) do`
+#     count[array[i]] ← count[array[i]] + 1
+#   for i = 2 to k do
+#     count[i] ← count[i] + count[i - 1]
+#   for i = length(array) downto 1 do
+#     output[count[array[i]]] ← array[i]
+#     count[array[i]] ← count[array[i]] - 1
+#   return output
